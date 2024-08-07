3169ec143af06feedd42c69928bbef94a92a0236
Abhishek <abhishekdhimaniitg@gmail.com>
2024-08-07 21:06:03 +0900

    Constants.kt file added

---
+++ app/src/main/java/com/example/chattingApp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.chattingApp.utils
+
+import com.example.chattingApp.BuildConfig
+
+
+const val WEB_CLIENT_ID = BuildConfig.GOOGLE_WEB_CLIENT_ID
+const val START_DESTINATION = "startDestination"
