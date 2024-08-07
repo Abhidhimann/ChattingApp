a33c1bc7f9061ff5b6ffe432a8f1d15ad886583e
Abhishek <abhishekdhimaniitg@gmail.com>
2024-08-07 14:19:02 +0900

    app icon added

--- app/src/main/java/com/example/chattingApp/ui/screens/splashscreen/StartUpLoading.kt
+++ app/src/main/java/com/example/chattingApp/ui/screens/splashscreen/StartUpLoading.kt
@@ -2,26 +2,37 @@ package com.example.chattingApp.ui.screens.splashscreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.chattingApp.R
 
 @Composable
 fun StartUpLoading() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.surface),
-        contentAlignment = Alignment.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // later replace it with app icon
-        Image(painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "null")
+        Spacer(modifier = Modifier.weight(1f))
+        Image(painterResource(id = R.mipmap.app_icon_foreground), contentDescription = "null")
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = "By")
+        Text(text = "Abhishek", style = MaterialTheme.typography.titleLarge, color = MaterialTheme.colorScheme.onPrimaryContainer, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(40.dp))
     }
 }
 
